ab87a7d04c890705c750dea8ed2aa753ed219bff
kevingardner <kjg5556@psu.edu>
2019-03-16 19:06:12 -0400

    - Add image detection of playing cards - Display name of image when detected

--- HackPSU-AR/ViewController.swift
+++ HackPSU-AR/ViewController.swift
@@ -13,6 +13,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var playingCardLabel: UILabel!
+    @IBOutlet weak var playingCardLabelView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -33,8 +35,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        playingCardLabelView.alpha = 0.0
+        playingCardLabelView.layer.cornerRadius = 10
+        
+        // Create set of detection images from AR Resources folder in the Assets.xcassets folder
+        guard let detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+        
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        // Add detection images to the ARConfiguration
+        configuration.trackingImages = detectionImages
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,15 +61,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
+        
+        // Check for a detected image
+        if let imageAnchor = anchor as? ARImageAnchor {
+            // Store the name of the detected image
+            let referenceImageName = imageAnchor.referenceImage.name
+            
+            // Asynchronously dispatch to the main thread (from a background thread) to update UI elements
+            DispatchQueue.main.async {
+                // Make the playing card label view visible
+                self.playingCardLabelView.alpha = 1.0
+                
+                // Set the text of the playing card label to the name of the detected image, which should be
+                //  name of the playing card
+                self.playingCardLabel.text = referenceImageName
+            }
+        }
      
         return node
     }
-*/
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
